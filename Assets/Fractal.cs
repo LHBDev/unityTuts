d73be9d56426706096937b77b36848b29e102f84
Ruben Baeza <rbaeza@tableau.com>
2016-11-14 16:00:36 -0600

    Forward and back vectors

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -9,6 +9,27 @@ public class Fractal : MonoBehaviour {
     public float childScale;
 
     private int depth;
+    private static Vector3[] childDirections =
+    {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back,
+        //special consideration since only added to root
+        Vector3.down
+    };
+
+    private static Quaternion[] childOrientations =
+    {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f),
+        //down only on root
+        Quaternion.Euler(0f, 0f, 180f)
+    };
 
     private void Start()
     {
@@ -22,17 +43,20 @@ public class Fractal : MonoBehaviour {
 
     private IEnumerator CreateChildren()
     {
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left);
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.down);
+        for (int i = 0; i < childDirections.Length - 1; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            new GameObject("FractalChild").AddComponent<Fractal>().Initialize(this, i);
+        }
+
+        if (this.depth == 0)
+        {
+            yield return new WaitForSeconds(0.5f);
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, childDirections.Length - 1);
+        }
     }
 
-    private void Initialize (Fractal parent, Vector3 direction)
+    private void Initialize (Fractal parent, int i)
     {
         mesh = parent.mesh;
         mat = parent.mat;
@@ -41,6 +65,7 @@ public class Fractal : MonoBehaviour {
         childScale = parent.childScale;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+        transform.localPosition = childDirections[i] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[i];
     }
 }
